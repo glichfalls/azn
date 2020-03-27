18b6b3c20a88128c9f69c49069b8da77a30b6ab7
Robin Portner <robin.portner@hotmail.com>
2020-03-27 19:33:25 +0100

    css changes

--- single-team.php
+++ single-team.php
@@ -1,8 +1,68 @@
 <?php get_header(); ?>
 
+<?php
+
+    $team = get_fields(get_the_ID());
+
+?>
+
 <main>
     
-    <?php include('inc/content.php') ?>
+    <?php if(have_posts()): the_post(); ?>
+
+        <div class="container">
+
+            <div class="page-title">
+                <h1><?= get_the_title() ?></h1>
+            </div>
+
+            <div class="content">
+                
+                <div class="gutenberg">
+                    <?= get_the_content() ?>
+                </div>
+
+                <h3 class="subtitle">Roster</h3>
+
+                <div class="team-grid">
+        
+                    <?php foreach($team['players'] as $player): ?>
+            
+                        <?php $fields = get_fields($player['player']->ID); ?>
+
+                        <div class="team-grid-item">
+
+                            <img src="<?= $fields['avatar']['url'] ?>" alt="" />
+
+                            <h4>
+                                <?= $player['player']->post_title ?>
+                                <span class="separator"></span>
+                                <?= $player['role'] ?>
+                            </h4>
+
+                        </div>
+        
+                    <?php endforeach; ?>
+
+                </div>
+
+                <h3 class="subtitle">Management</h3>
+
+                <div class="team-grid">
+        
+                    <?php foreach($team['management'] as $player): ?>
+        
+        
+        
+                    <?php endforeach; ?>
+
+                </div>
+                
+            </div>
+            
+        </div>
+    
+    <?php endif; ?>
 
 </main>
 
